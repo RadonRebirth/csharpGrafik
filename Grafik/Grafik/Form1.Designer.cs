1051556d61761a5ea7fe29edf9936cf4e002bdda
RadonRebirth <96480522+RadonRebirth@users.noreply.github.com>
2023-03-31 09:54:28 +0300

    Add files via upload

---
+++ Grafik/Grafik/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Grafik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            trackBarA = new TrackBar();
+            trackBarB = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarA).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarB).BeginInit();
+            SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(59, 39);
+            chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(464, 325);
+            chart1.TabIndex = 0;
+            chart1.Text = "chart1";
+            // 
+            // trackBarA
+            // 
+            trackBarA.Location = new Point(644, 157);
+            trackBarA.Minimum = -10;
+            trackBarA.Name = "trackBarA";
+            trackBarA.Size = new Size(104, 45);
+            trackBarA.TabIndex = 1;
+            // 
+            // trackBarB
+            // 
+            trackBarB.Location = new Point(644, 232);
+            trackBarB.Minimum = -10;
+            trackBarB.Name = "trackBarB";
+            trackBarB.Size = new Size(104, 45);
+            trackBarB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(547, 166);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Значение a";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(547, 232);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Значение b";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(trackBarB);
+            Controls.Add(trackBarA);
+            Controls.Add(chart1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarA).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarB).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private TrackBar trackBarA;
+        private TrackBar trackBarB;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Grafik/Grafik/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Grafik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(trackBarA.Value) / 10.0;
+            double b = Convert.ToDouble(trackBarB.Value) / 10.0;
+
+            chart1.ChartAreas[0].AxisX.Minimum = -10;
+            chart1.ChartAreas[0].AxisX.Maximum = 10;
+            chart1.ChartAreas[0].AxisY.Minimum = -2;
+            chart1.ChartAreas[0].AxisY.Maximum = 2;
+
+            Series series = new Series("y=a*sin(bx)");
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 3;
+
+            for (double x = -10; x <= 10; x += 0.01)
+            {
+                double y = a * Math.Sin(b * x);
+                series.Points.AddXY(x, y);
+            }
+            chart1.Series.Clear();
+            chart1.Series.Add(series);
+
+            trackBarA.Scroll += new EventHandler(trackBarA_Scroll);
+            trackBarB.Scroll += new EventHandler(trackBarB_Scroll);
+        }
+        public void trackBarA_Scroll(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(trackBarA.Value) / 10.0;
+            double b = Convert.ToDouble(trackBarB.Value) / 10.0;
+
+            Series series = new Series("y=a*sin(bx)");
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 3;
+            for (double x = -10; x <= 10; x += 0.01)
+            {
+                double y = a * Math.Sin(b * x);
+                series.Points.AddXY(x, y);
+            }
+
+            chart1.Series.Clear();
+            chart1.Series.Add(series);
+
+            chart1.Invalidate();
+
+        }
+        public void trackBarB_Scroll(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(trackBarA.Value) / 10.0;
+            double b = Convert.ToDouble(trackBarB.Value) / 10.0;
+
+            Series series = new Series("y=a*sin(bx)");
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 3;
+            for (double x = -10; x <= 10; x += 0.01)
+            {
+                double y = a * Math.Sin(b * x);
+                series.Points.AddXY(x, y);
+            }
+
+            chart1.Series.Clear();
+            chart1.Series.Add(series);
+
+            chart1.Invalidate();
+        }
+    }
+}
